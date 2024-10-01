b2165205f4878cf603f1c798380747a283aa7c8a
Pranay Suyash <pranay.suyash@gmail.com>
2024-10-01 12:06:33 +0530

    cleaned commented codes

--- src/components/pages/Upload.jsx
+++ src/components/pages/Upload.jsx
@@ -1,47 +1,4 @@
-// // src/components/pages/Upload.jsx
-// import React, { useState } from 'react';
-// import styled from 'styled-components';
-// import FileUploader from '../common/FileUploader';
-// import Notification from '../common/Notification';
-
-// const UploadContainer = styled.div`
-//   padding: 20px;
-// `;
-
-// function Upload() {
-//   const [notification, setNotification] = useState({ message: '', type: 'success' });
-//   const [uploadedFiles, setUploadedFiles] = useState([]);
-
-//   return (
-//     <UploadContainer>
-//       <h1>Upload Files</h1>
-//       {notification.message && (
-//         <Notification 
-//           message={notification.message} 
-//           type={notification.type} 
-//           onClose={() => setNotification({ message: '', type: 'success' })} 
-//         />
-//       )}
-//       <FileUploader 
-//         setUploadedFiles={setUploadedFiles} 
-//         setNotification={setNotification} 
-//       />
-//       {}
-//       {uploadedFiles.length > 0 && (
-//         <div>
-//           <h2>Uploaded Files:</h2>
-//           <ul>
-//             {uploadedFiles.map((file, index) => (
-//               <li key={index}>{file.filename}</li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-//     </UploadContainer>
-//   );
-// }
-
-// export default Upload;
+// src/components/pages/Upload.jsx
 
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
